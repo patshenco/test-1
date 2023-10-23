5073a46591c5fd4a36ba8a9f7cd0b415f9e5a367
Fouham <141226918+patshenco@users.noreply.github.com>
2023-10-23 15:48:56 +0100

    test1

--- routes/categories.js
+++ routes/categories.js
@@ -1,37 +1,120 @@
-const Category = require('../models/category')
+const Category = require("../models/category");
 
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 
-const route = express.Router()
 
-route.post('/categories', async (req, res) => {
-    try {
-      const { categoryName } = req.body;
-  
-      // Check if the category name already exists
-      const existingCategory = await Category.findOne({ categoryName });
-  
-      if (existingCategory) {
-        return res.status(400).json({ status: 400, message: `The category '${categoryName}' already exists` });
-      }
-  
-      // Create a new Category instance
-      const newCategory = new Category({
-        categoryName,
-      });
-  
-      // Save the new Category to the database
-      const savedCategory = await newCategory.save();
-  
-      res.status(201).json(savedCategory);
-    } catch (error) {
-      console.error('Error creating Category:', error);
-      res.status(500).json({ status: 500, message: 'Internal Server Error' });
+const route = express.Router();
+
+route.post("/categories", async (req, res) => {
+  try {
+    const { categoryName } = req.body;
+
+    // Check if the category name already exists
+    const existingCategory = await Category.findOne({ categoryName });
+
+    if (existingCategory) {
+      return res
+        .status(400)
+        .json({
+          status: 400,
+          message: `The category '${categoryName}' already exists`,
+        });
     }
-  });
-  
+
+    // Create a new Category instance
+    const newCategory = new Category({
+      categoryName,
+    });
+
+    // Save the new Category to the database
+    const savedCategory = await newCategory.save();
+
+    res.status(201).json(savedCategory);
+  } catch (error) {
+    console.error("Error creating Category:", error);
+    res.status(500).json({ status: 500, message: "Internal Server Error" });
+  }
+});
+
+route.get("/categories", async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = 10;
+    const skip = (page - 1) * limit;
+
+    const categories = await Category.find().skip(skip).limit(limit);
+    const totalCategories = await Category.countDocuments(); // Get the total number of categories
+
+    // Check if there are no categories
+    if (categories.length === 0) {
+      return res.status(200).json({ status: 200, data: [], total: totalCategories, page });
+    }
+
+    res.status(200).json({ status: 200, data: categories, total: totalCategories, page });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: 500, message: "Internal Server Error" });
+  }
+});
+
+route.get("/categories/search", async (req, res) => {
+  try {
+    const query = req.query.query; 
+    const page = parseInt(req.query.page) || 1; 
+    const limit = 10; 
+    const skip = (page - 1) * limit;
+
+    
+    const categories = await Category.find({ categoryName: { $regex: query, $options: 'i' } })
+      .skip(skip)
+      .limit(limit);
+
+    // Check if there are no categories
+    if (categories.length === 0) {
+      return res.status(200).json({ status: 200, data: [] });
+    }
+
+    const totalCategories = await Category.countDocuments({ categoryName: { $regex: query, $options: 'i' } }); 
+    res.status(200).json({
+      status: 200,
+      data: categories,
+      total: totalCategories,
+      page: page,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: 500, message: "Internal Server Error" });
+  }
+});
+
+route.get("/categories/:id", async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(categoryId)) {
+      return res.status(400).json({
+        status: 400,
+        message: "Invalid category ID",
+      });
+    }
+
+    const category = await Category.findById(categoryId);
+
+    if (!category) {
+      return res.status(404).json({
+        status: 404,
+        message: "Category not found",
+      });
+    }
+
+    res.status(200).json({ status: 200, data: category });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: 500, message: "Internal Server Error" });
+  }
+});
 
 
-  
 
-  module.exports = route
+module.exports = route;
